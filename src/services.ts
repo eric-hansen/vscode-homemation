d2cc0f309196f511c9fcf086661ee700ad3afa07
Eric Hansen <hansen.c.eric@gmail.com>
2017-10-08 12:28:00 -0400

    Abstract base class all other instances need to inherit from

---
+++ src/services.ts
@@ -0,0 +1,31 @@
+'use strict'
+
+import { ConfigManagement } from './utils/config_management'
+
+export class AbstractServices {
+
+  public config: ConfigManagement
+  private readonly rootKeyForConfig = 'homemation'
+
+  initConfig(service?: AbstractServices) {
+    this.config = new ConfigManagement(service)
+  }
+
+  /**
+   * Root key for configuration.  This should never change.
+   */
+  getRootConfig() {
+    return this.rootKeyForConfig
+  }
+
+  /**
+   * Sectional config key (i.e.: 'hubs' for the hub devices)
+   */
+  getSectionConfig() {}
+  
+  /**
+   * Any extra elements of the config key prior to the config element itself.
+   * I.e.: 'philips_hue' for the Philips Hue devices
+   */
+  getConfigKey() {}
+}
